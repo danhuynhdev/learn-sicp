8fbbc0ea11d316756f9ae4b0ac9671b9c4e58c46
Huynh Tan <danhuynhdev@gmail.com>
2016-06-17 13:13:12 +0700

    Done ex 2.7 to 2.12

--- 1.rkt
+++ 1.rkt
@@ -948,3 +948,101 @@
   (lambda (f)
 	(lambda (x)
 	  ((a f) ((b f) x)))))
+
+;;; Extended Exercise: Interval Arithmetic
+
+(define (add-interval x y)
+  (make-interval (+ (lower-bound x) (lower-bound y))
+				 (+ (upper-bound x) (upper-bound y))))
+
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+		(p2 (* (lower-bound x) (upper-bound y)))
+		(p3 (* (upper-bound x) (lower-bound y)))
+		(p4 (* (upper-bound x) (upper-bound y))))
+	(make-interval (min p1 p2 p3 p4)
+				   (max p1 p2 p3 p4))))
+
+;; (define (div-interval x y)
+;;   (mul-interval
+;;    x
+;;    (make-interval (/ 1.0 (upper-bound y))
+;; 				  (/ 1.0 (lower-bound y)))))
+
+(define (make-interval a b) (cons a b))
+
+;;; 2.7
+
+(define (lower-bound i) (min (car i) (cdr i)))
+(define (upper-bound i) (max (cdr i) (cdr i)))
+
+;;; 2.8
+
+(define (sub-interval x y)
+  (add-interval x
+				(make-interval (- (upper-bound y))
+							   (- (lower-bound y)))))
+
+;;; 2.9
+
+;; (define (width i)
+;;   (/ (- (upper-bound i) (lower-bound i)) 2))
+
+;;; This is some basic math
+;;; (width (add-interval a b)) ->
+;;; (/ (- (+ (lower-bound a) (lower-bound b))
+;;;       (+ (upper-bound a) (upper-bound b)))
+;;;    2) ->
+;;; (- (/ (+ (lower-bound a) (lower-bound b))
+;;;       2)
+;;;    (/ (+ (upper-bound a) (upper-bound b))
+;;;       2)) ->
+;;; (- (+ (lower-bound (/ a 2)) (lower-bound (/ b 2)))
+;;;    (+ (upper-bound (/ a 2)) (upper-bound (/ b 2))))
+
+;;; 2.10
+
+(define (div-interval x y)
+  (if (<= (* (upper-bound y) (lower-bound y)) 0)
+	  (error "Divide by an interval that spans zero")
+	  (mul-interval
+	   x
+	   (make-interval (/ 1.0 (upper-bound y))
+					  (/ 1.0 (lower-bound y))))))
+
+(define (make-center-width c w)
+  (make-interval (- c w) (+ c w)))
+(define (center i)
+  (/ (+ (lower-bound i) (upper-bound i)) 2))
+(define (width i)
+  (/ (- (upper-bound i) (lower-bound i)) 2))
+
+;;; 2.12
+
+(define (make-center-percent c p)
+  (let ((w (/ (* c p) 100.)))
+	(make-center-width c w)))
+
+(define (percent i)
+  (* 100 (/ (width i) (center i))))
+
+;;; 2.13
+;;; math stuff don't care :|
+
+
+(define (par1 r1 r2)
+  (div-interval (mul-interval r1 r2)
+				(add-interval r1 r2)))
+(define (par2 r1 r2)
+  (let ((one (make-interval 1 1)))
+	(div-interval
+	 one (add-interval (div-interval one r1)
+					   (div-interval one r2)))))
+
+;;; 2.14
+
+(define r1 (make-center-percent 10 1))
+(define r2 (make-center-percent 20 1))
+
+;; (par1 r1 r2) --> [6.4693..., 6.8693...]
+;; (par2 r1 r2) --> [6.6, 6.733...]
