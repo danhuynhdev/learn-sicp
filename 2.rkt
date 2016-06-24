076e7f1f9d1350c84ce0d00d266f937e5022f5c0
Huynh Tan <danhuynhdev@gmail.com>
2016-06-24 23:14:30 +0700

    Done ex 2.53 2.54 2.55 2.56 2.57

--- 2.rkt
+++ 2.rkt
@@ -1181,3 +1181,127 @@
   (let ((combine4 (square-of-four flip-vert rotate180
 								  identity flip-horiz)))
 	(combine4 (corner-split painter n))))
+
+;;; Symbolic Data
+
+;;; Quotation
+
+;;; 2.53
+
+;; (list 'a 'b 'c) -> (a b c)
+;; (list (list 'george)) -> ((george))
+;; (cdr '((x1 x2) (y1 y2))) -> ((y1 y2))
+;; (cadr '((x1 x2) (y1 y2))) -> (y1 y2)
+;; (pair? (car '(a short list))) -> #f
+;; (memq 'red '((red shoes) (blue socks))) -> #f
+;; (memq 'red '(red shoes blue socks)) -> (red shoes blue socks)
+
+;;; 2.54
+
+;; (define (equal? a b)
+;;   (cond ((and (pair? a) (pair? b)) (and (equal? (car a)
+;; 												(car b))
+;; 										(equal? (cdr a)
+;; 												(cdr b))))
+;; 		((or (pair? a) (pair? b)) false)
+;; 		(else (eq? a b))))
+
+;;; 2.55
+;;; (car ''abracadabra)
+;;; (car (quote (quote abracadabra)))
+;;; (car '(quote abracadabra))
+
+;;; Example: Symbolic Differentiation
+
+;;; The differentiation program with abstract data
+
+;; (define (deriv exp var)
+;;   (cond ((number? exp) 0)
+;; 		((variable? exp) (if (same-variable? exp var) 1 0))
+;; 		((sum? exp) (make-sum (deriv (addend exp) var)
+;; 							  (deriv (augend exp) var)))
+;; 		((product? exp)
+;; 		 (make-sum
+;; 		  (make-product (multiplier exp)
+;; 						(deriv (multiplicand exp) var))
+;; 		  (make-product (deriv (multiplier exp) var)
+;; 						(multiplicand exp))))
+;; 		(else
+;; 		 (display "unknown expression type: DERIV" exp))))
+
+;;; Representing algebraic expressions
+
+(define (variable? x) (symbol? x))
+(define (same-variable? v1 v2)
+  (and (variable? v1) (variable? v2) (eq? v1 v2)))
+;; (define (make-sum a1 a2) (list '+ a1 a2))
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+		((=number? a2 0) a1)
+		((and (number? a1) (number? a2))
+		 (+ a1 a2))
+		(else (list '+ a1 a2))))
+(define (=number? exp num) (and (number? exp) (= exp num)))
+;; (define (make-product m1 m2) (list '* m1 m2))
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+		((=number? m1 1) m2)
+		((=number? m2 1) m1)
+		((and (number? m1) (number? m2)) (* m1 m2))
+		(else (list '* m1 m2))))
+(define (sum? x) (and (pair? x) (eq? (car x) '+)))
+(define (addend s) (cadr s))
+;; (define (augend s) (caddr s))
+(define (product? x) (and (pair? x) (eq? (car x) '*)))
+(define (multiplier p) (cadr p))
+;; (define (multiplicand p) (caddr p))
+
+;;; 2.56
+
+(define (make-exponentiation u n)
+  (cond ((=number? n 0) 1)
+		((=number? n 1) u)
+		(else (list '** u n))))
+(define (base e) (cadr e))
+(define (exponent e) (caddr e))
+(define (exponentiation? x) (and (pair? x) (eq? (car x) '**)))
+
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+		((variable? exp) (if (same-variable? exp var) 1 0))
+		((sum? exp) (make-sum (deriv (addend exp) var)
+							  (deriv (augend exp) var)))
+		((product? exp)
+		 (make-sum
+		  (make-product (multiplier exp)
+						(deriv (multiplicand exp) var))
+		  (make-product (deriv (multiplier exp) var)
+						(multiplicand exp))))
+		((exponentiation? exp)
+		 (make-product (make-product
+						(exponent exp)
+						(make-exponentiation (base exp)
+											 (make-sum (exponent exp)
+													   -1)))
+					   (deriv (base exp) var)))
+		(else
+		 (display "unknown expression type: DERIV" exp))))
+
+;;; 2.57
+
+(define (augend s)
+  (define (sum a)
+	(if (= (length a) 2)
+		(list '+ (car a) (cadr a))
+		(make-sum (car a) (sum (cdr a)))))
+  (if (= (length (cddr s)) 1)
+	  (caddr s)
+	  (sum (cddr s))))
+(define (multiplicand p)
+  (define (product m)
+	(if (= (length m) 2)
+		(list '* (car m) (cadr m))
+		(make-product (car m) (product (cdr m)))))
+  (if (= (length (cddr p)) 1)
+	  (caddr p)
+	  (product (cddr p))))
