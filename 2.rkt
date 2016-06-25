0a3fb518ef4c5e167f3c46c14eb07926dafbe0d1
Huynh Tan <danhuynhdev@gmail.com>
2016-06-25 20:56:24 +0700

    Done ex 2.59 2.69 2.61 2.62

--- 2.rkt
+++ 2.rkt
@@ -1372,3 +1372,90 @@
 		m)))
 
 ;;; Representing Sets
+
+;; ;;; Sets as unordered lists
+;; (define (element-of-set? x set)
+;;   (cond ((null? set) false)
+;; 		((equal? x (car set)) true)
+;; 		(else (element-of-set? x (cdr set)))))
+;; (define (adjoin-set x set)
+;;   (if (element-of-set? x set)
+;; 	  set
+;; 	  (cons x set)))
+;; (define (intersection-set set1 set2)
+;;   (cond ((or (null? set1) (null? set2)) '())
+;; 		((element-of-set? (car set1) set2)
+;; 		 (cons (car set1) (intersection-set (cdr set1) set2)))
+;; 		(else (intersection-set (cdr set1) set2))))
+
+;;; 2.59
+
+;; (define (union-set set1 set2)
+;;   (cond ((null? set1) set2)
+;; 		((null? set2) set1)
+;; 		((element-of-set? (car set1) set2)
+;; 		 (union-set (cdr set1) set2))
+;; 		(else (cons (car set1) (union-set (cdr set1) set2)))))
+
+;;; 2.60
+
+;; (define (adjoin-set-dup x set)
+;;   (cons x set))
+
+;; (define (union-set-dup set1 set2) (append set1 set2))
+
+;;; adjoin-set now grow as O(1)
+;;; intersection-set is the same because we still have to use element-of-set?
+;;; union-set now grow as O(n)
+
+;;; these thing may be useful if we don't have to care about memory and just
+;;; care about if a thing is in the list or not
+
+;;; Sets as ordered lists
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+		((= x (car set)) true)
+		((< x (car set)) false)
+		(else (element-of-set? x (cdr set)))))
+(define (intersection-set set1 set2)
+  (if (or (null? set1) (null? set2))
+	  '()
+	  (let ((x1 (car set1)) (x2 (car set2)))
+		(cond ((= x1 x2)
+			   (cons x1 (intersection-set (cdr set1)
+										  (cdr set2))))
+			  ((< x1 x2)
+			   (intersection-set (cdr set1) set2))
+			  ((< x2 x1)
+			   (intersection-set set1 (cdr set2)))))))
+
+;;; 2.61
+
+(define (adjoin-set x set)
+  (if (null? set)
+	  '()
+	  (let ((y (car set)))
+		(cond ((= x y) set)
+			  ((< x y)
+			   (cons x set))
+			  ((and (> x y) (null? (cdr set)))
+			   (cons y (cons x nil)))
+			  (else (cons y (adjoin-set x (cdr set))))))))
+
+;;; 2.62
+
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+		((null? set2) set1)
+		(else
+		 (let ((x1 (car set1)) (x2 (car set2)))
+		   (cond ((= x1 x2)
+				  (cons x1 (union-set (cdr set1)
+											 (cdr set2))))
+				 ((< x1 x2)
+				  (cons x1 (union-set (cdr set1) set2)))
+				 ((< x2 x1)
+				  (cons x2 (union-set set1 (cdr set2)))))))))
+
+;;; Sets as binary trees
